a99b4e96665edaef82b9c328ef58c6266308aa7b
Lucas Santos Silva <luss1010@gmail.com>
2025-11-17 15:40:37 -0300

    refactor: removing print

--- app_c_rest_grpc/client.py
+++ app_c_rest_grpc/client.py
@@ -6,7 +6,6 @@ def get_sticker(port: int = 50051):
         stub = sticker_pb2_grpc.StickerServiceStub(channel)
         try:
             result = stub.GetSticker(sticker_pb2.StickerRequest())
-            print(f'{result}')
             return result.name
         except grpc.RpcError as e:
             print(f'{e}')
